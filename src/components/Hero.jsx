6539fef5b87a90b58afe00ff82505077c95c2a91
vivekdobariya <vivekdobariya333@gmail.com>
2024-04-01 17:22:24 +0530

    add AI summarize with complete all functionality

---
+++ src/components/Hero.jsx
@@ -0,0 +1,29 @@
+import { logo } from "../assets";
+
+const Hero = () => {
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="w-full flex justify-between items-center mb-10 pt-3">
+        <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
+        <button
+          type="button"
+          className="black_btn"
+          onClick={() => window.open("https://github.com/vivekdobariya")}
+        >
+          Github
+        </button>
+      </nav>
+
+      <h1 className="head_text">
+        Summarize Article With <br className="max-md: hidden" />
+        <span className="orange_gradient">OpenAI Gpt-4</span>
+      </h1>
+
+      <h2 className="desc">
+        Simplify your reading with Summarize, an open-source article summarizer
+        that transforms lengthy articles into clear and concise summaries
+      </h2>
+    </header>
+  );
+};
+export default Hero;
